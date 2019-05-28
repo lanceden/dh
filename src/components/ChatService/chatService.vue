25899554e4184c1611e8152e963a21141be0a419
鄧羽翔 <lance.den@outlook.com>
2019-05-29 00:33:39 +0800

    2019年5月29日
    1. 調整文章客服錯誤

--- src/components/ChatService/chatService.vue
+++ src/components/ChatService/chatService.vue
@@ -1,7 +1,22 @@
 <template>
   <div>
     <loading v-show="true" />
-    連線文字客服中...
+    <div class="content">
+      <section class="content-header">
+        <h1>
+          <p><i aria-hidden="true" class="fa fa-meh-o fa-6"></i></p>
+        </h1>
+        <ol class="breadcrumb">
+          <li></li>
+          <li class="active">
+            <p style="color: red;">連線文字客服中...</p>
+          </li>
+        </ol>
+      </section>
+      <section class="content">
+        <h2></h2>
+      </section>
+    </div>
   </div>
 </template>
 
@@ -14,22 +29,31 @@ export default {
   },
   methods: {
     dosubmit() {
+      var self = this
       let domainServer = `https://cti.skl.com.tw/mmccmedia/`
       let [CustName, CustIDN] = [getQueryStringParameterByKey('CustName'), getQueryStringParameterByKey('CustIDN')]
       window.jQuery.ajax({
-        url: domainServer + 'ajax/jsonp/domainInfo/getWorkTime/',
+        url: `${domainServer}ajax/jsonp/domainInfo/getWorkTime/`,
         dataType: 'jsonp'
-      }).done(function(data) {
+      }).done(data => {
         if ((data) && (data.reply) && (window.jQuery.trim(data.reply) === 'YES')) {
           if (CustName.length > 0 && CustIDN.length > 0) {
             let gender = CustIDN.substr(1, 1) === '1' ? '男' : '女'
             let result = `${domainServer}html/chat-service.html?customerName=${CustName}&customerGender=${gender}&customerQuestion=網路投保`
             window.location.href = result
           } else {
-            toggleModalShow('親愛的保戶您好，請登入後在使用文字客服。')
+            toggleModalShow('親愛的保戶您好，請登入後在使用文字客服。', '貼心提醒您')
+            setTimeout(() => {
+              self.$router.push(`/leave=true`)
+              window.location.reload()
+            }, 2000)
           }
         } else {
           toggleModalShow('親愛的保戶您好，線上客服開放時間為: 週一至週五上午8點30分至晚上5點30分; 請於開放時間使用或直撥免費服務電話', '貼心提醒您')
+          setTimeout(() => {
+            self.$router.push(`/leave=true`)
+            window.location.reload()
+          }, 2000)
         }
       })
     }
